c9a07b3a4d366f1d6e1c1ac476d83df0cfab890d
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-10-29 16:52:50 +0530

    First Commit

---
+++ Sending_Mail/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Sending_Mail
+{
+    public partial class MainPage : ContentPage
+    {
+        SmtpClient smtpserver;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public  void btnSendMail_Clicked(object sender, EventArgs e)
+        {
+            
+          List<string> toAddress = new List<string>();
+          toAddress.Add(txtEmail.Text);
+          SendEmail(txtSubject.Text, txtBody.Text, toAddress);
+          
+        }
+
+        public void SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                };
+                 Email.ComposeAsync(message);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mail Failed", ex.Message, "OK");
+            }
+        }
+    }
+}
